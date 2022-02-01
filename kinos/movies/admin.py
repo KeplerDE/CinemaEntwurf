269a88f30e25b44421a782ef83501049ec495ac9
KeplerDE <90717368+KeplerDE@users.noreply.github.com>
2022-02-01 11:30:28 +0300

    Admin Bildschirmbild

--- kinos/movies/admin.py
+++ kinos/movies/admin.py
@@ -16,22 +16,34 @@ class ReviewInline(admin.TabularInline):
     extra = 1
     readonly_fields = ("name", "email")
 
+
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+    get_image.short_description = "Изображение"
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     """Фильмы"""
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    readonly_fields = ("get_image",)
     fieldsets = (
         (None, {
             "fields": (("title", "tagline"),)
         }),
         (None, {
-            "fields": ("description", "poster")
+            "fields": ("description", ("poster", "get_image"))
         }),
         (None, {
             "fields": (("year", "world_premiere", "country"),)
@@ -48,6 +60,11 @@ class MovieAdmin(admin.ModelAdmin):
         }),
     )
 
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
+
+    get_image.short_description = "Постер"
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
